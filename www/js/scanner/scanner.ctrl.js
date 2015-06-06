8389ba34bead7480aa1c28dc8a4e5a3c2ed9b131
Claudiu Filip <claudiucelfilip@gmail.com>
2015-06-06 12:51:25 +0300

    - added modal and popups to scanner

--- www/js/scanner/scanner.ctrl.js
+++ www/js/scanner/scanner.ctrl.js
@@ -1,28 +1,66 @@
 angular.module('alt.scanner', [])
-  .controller('ScannerCtrl', function ($scope, $cordovaBarcodeScanner, $ionicModal, Auth) {
+  .controller('ScannerCtrl', function ($scope, $cordovaBarcodeScanner, $ionicPopup,  $ionicModal, Auth) {
     //Auth.check();
     $scope.loading = true;
 
-
+  try {
     $cordovaBarcodeScanner
       .scan()
-      .then(function(imageData) {
+      .then(function (imageData) {
         $scope.loading = false;
-        alert(imageData.text);
+        $scope.showPopup();
 
-      }, function(error) {
+      }, function (error) {
         $scope.loading = false;
         console.log('error', error);
       });
 
+  } catch(e) {
+    console.log(e);
+  }
 
-    $ionicModal.fromTemplateUrl('modal.html', {
+    $scope.showPopup = function () {
+      $scope.data = {};
+
+      // An elaborate, custom popup
+      var myPopup = $ionicPopup.show({
+        template: '',
+        title: 'Congratulations',
+        subTitle: 'You have won 1300 points',
+        scope: $scope,
+        buttons: [
+          {text: 'Cancel'},
+          {
+            text: '<b>Save</b>',
+            type: 'button-positive',
+            onTap: function (e) {
+              if (!$scope.data.wifi) {
+                //don't allow the user to close unless he enters wifi password
+                e.preventDefault();
+              } else {
+                return $scope.data.wifi;
+              }
+            }
+          }
+        ]
+      });
+
+      myPopup.then(function (res) {
+        console.log('Tapped!', res);
+      });
+      $timeout(function () {
+        myPopup.close(); //close the popup after 3 seconds for some reason
+      }, 3000);
+    };
+
+
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
       scope: $scope,
       animation: 'slide-in-up'
     }).then(function(modal) {
       $scope.modal = modal;
     });
-
     $scope.openModal = function() {
       $scope.modal.show();
     };
